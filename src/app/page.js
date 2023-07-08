48907e290fa614090d82381a03c1140f3614e18b
bovdaboat <ignatgabov@gmail.com>
2023-07-07 20:03:40 -0700

    working on the hero section of the app

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Navbar from "@/app/Components/Navbar";
+import Hero from "@/app/Components/Hero";
 
 export default function Home() {
   return (
     <>
     <main>
       <Navbar />
+      <Hero />
     </main>
     </>
   )
